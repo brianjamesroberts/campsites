361a0bf45f4fb891a2f197e1481ccb628ff76027
Brian Roberts <brianjamesroberts@gmail.com>
2016-10-24 20:33:58 -0700

    first commit

---
+++ app/src/main/java/com/unfairtools/campsites/dagger/component/SQLiteModule.java
@@ -0,0 +1,8 @@
+package com.unfairtools.campsites.dagger.component;
+
+/**
+ * Created by newuser on 10/24/16.
+ */
+
+public class SQLiteModule {
+}
---
+++ app/src/main/java/com/unfairtools/campsites/dagger/component/ServicesComponent.java
@@ -0,0 +1,23 @@
+package com.unfairtools.campsites.dagger.component;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by newuser on 10/24/16.
+ */
+
+
+
+@Component(modules = {
+        SQLiteModule.class
+})
+
+@Singleton
+public interface ServicesComponent {
+
+    SQLiteDatabase sqlDatabase();
+}
